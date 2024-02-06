58062fb1111547f2ba6d905a3d3c60957bce0fcb
7vTr0x7 <7vtrox7@gmail.com>
2024-02-06 20:01:36 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import "./App.css"
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+
+/*
+   Head
+   Body
+    - SideBar
+    - MenuItems
+   MenuContainer
+    - ButtonsList
+    - VideoContainer
+       - VideoCard 
+*/
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Head />
+      <Body />
     </div>
   );
 }
