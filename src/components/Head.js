bc6879071ffb7550cf92cea67898c9cb5afad27f
7vTr0x7 <7vtrox7@gmail.com>
2024-02-06 21:17:44 +0530

    Head

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import menu from "../utils/menu.png";
+import logo from "../utils/logo.png";
+import userIcon from "../utils/user.png";
+import searchIcon from "../utils/search.png";
 
 const Head = () => {
   return (
-    <div>Head</div>
-  )
-}
+    <div className="grid grid-flow-col p-5 m-2 shadow-xl">
+      <div className="flex col-span-1 ">
+        <img alt="menu" src={menu} className="h-8" />
+        <img alt="logo" src={logo} className="h-8 ml-5" />
+      </div>
+      <div className="col-span-10 flex justify-center">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
 
-export default Head
+        <img
+          alt="search"
+          src={searchIcon}
+          className="h-11 border border-gray-400  rounded-r-full py-2 px-5"
+        />
+      </div>
+      <div className="col-span-1">
+        <img alt="user" src={userIcon} className="h-8" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="p-5 shadow-xl w-48">
+    
+      <h1 className="font-bold">Home</h1>
+      <h1 className="font-bold">Shorts</h1>
+      <h1 className="font-bold">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
