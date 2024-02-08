7b2795a386fa1e64671fedb09ddeaf91b1553565
7vTr0x7 <7vtrox7@gmail.com>
2024-02-08 19:09:56 +0530

    comment section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
 
@@ -12,14 +13,20 @@ const WatchPage = () => {
   });
 
   return (
-    <div className="p-5 ml-24">
-      <iframe
-        class="w-[700px] h-[400px] rounded-2xl shadow-xl"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen></iframe>
+    <div>
+      <div className="p-5 ml-24">
+        <iframe
+          class="w-[700px] h-[400px] rounded-2xl shadow-xl"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen></iframe>
+
+        <div>
+          <CommentsContainer />
+        </div>
+      </div>
     </div>
   );
 };
