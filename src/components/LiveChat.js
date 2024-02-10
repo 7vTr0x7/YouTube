e77d21f93857f85f2dc7d637933b1ddacd0aeaff
7vTr0x7 <7vtrox7@gmail.com>
2024-02-10 20:18:43 +0530

    redux for live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateName, generateText } from "../utils/helper";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+
+  const chatMessages = useSelector((store) => store.chat.messages);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      dispatch(
+        addMessage({
+          name: generateName(),
+          message: generateText(10),
+        })
+      );
+    }, 2000);
+
+    return () => clearInterval(i);
+  }, []);
+
   return (
-    <div className="w-8/12 h-[400px] ml-10   p-2 border border-black rounded-xl bg-gray-100">
-      <div className="p-2">
-        <h1 className="font-bold">Live Chat</h1>
-      </div>
-      <ChatMessage name="vTr0x" message="hi!" />
-      <ChatMessage name="vTr0x" message="hi!" />
-      <ChatMessage name="vTr0x" message="hi!" />
-      <ChatMessage name="vTr0x" message="hi!" />
-      <ChatMessage name="vTr0x" message="hi!" />
+    <div className="w-8/12 h-[400px] ml-10   p-2 border border-black rounded-xl bg-gray-100 overflow-y-scroll  flex flex-col-reverse">
+      {chatMessages.map((message) => (
+        <ChatMessage
+          key={message.name}
+          name={message.name}
+          message={message.message}
+        />
+      ))}
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,6 @@ export const YT_VIDEO_API =
 
 export const YT_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+
+  export const LIVE_CHAT_COUNT = 10
