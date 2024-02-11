8435129d961041d948ddb75c2fdfd10ed7a777cd
7vTr0x7 <7vtrox7@gmail.com>
2024-02-11 19:59:33 +0530

    useMemo

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { getNthPrime } from "../utils/helper";
 
 const Demo = () => {
   const [num, setNum] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const prime = getNthPrime(num);
+  const prime = useMemo(() => getNthPrime(num), [num]);
 
   return (
-    <div className=" w-96 h-96 p-4 m-4  border border-black ">
+    <div
+      className={`w-96 h-96 p-4 m-4 bg-gray-50 border border-black ${
+        isDarkMode && "bg-gray-950 text-white"
+      }`}>
+      <button
+        className="bg-gray-100 rounded-xl font-semibold p-2 m-2 "
+        onClick={() => setIsDarkMode(!isDarkMode)}>
+        Toggle
+      </button>
       <div>
         <input
           className="border border-black w-11/12 px-2"
           type="number"
           value={num}
-          onChange={(e) => setNum(e.target.value)}
+          onChange={(e) => setNum(parseInt(e.target.value))}
         />
       </div>
-      <div>prime : {prime}</div>
+      <div>Prime: {prime}</div>
     </div>
   );
 };
